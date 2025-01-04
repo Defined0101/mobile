b12e04e7143167b38f4a9209cb74c50c03bc4115
elifatar <eatar@etu.edu.tr>
2025-01-04 14:28:47 +0300

    feat: data classes are added for FS [FRS-81]

---
+++ app/src/main/java/com/defined/mobile/ui/Preferences.kt
@@ -0,0 +1,9 @@
+package com.defined.mobile.ui
+
+data class Preferences(
+    val dairyFree: Boolean,
+    val glutenFree: Boolean,
+    val pescetarian: Boolean,
+    val vegan: Boolean,
+    val vegetarian: Boolean
+)
--- app/src/main/java/com/defined/mobile/ui/theme/Type.kt
+++ app/src/main/java/com/defined/mobile/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.defined.mobile.ui.theme
 
-import android.graphics.fonts.FontFamily
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
