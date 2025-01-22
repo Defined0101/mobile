9f2b9e1468d54980061f70d5ebc820ffae6e98a3
elifatar <eatar@etu.edu.tr>
2025-01-22 15:19:09 +0300

    clean: cleaned unnecessary imports

--- app/src/main/java/com/defined/mobile/MainActivity.kt
+++ app/src/main/java/com/defined/mobile/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.defined.mobile.ui.AllergyPage
 import com.defined.mobile.ui.IngredientSearch
-import com.defined.mobile.ui.Ingredients
 import com.defined.mobile.ui.theme.MobileTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/defined/mobile/ui/AllergyPage.kt
+++ app/src/main/java/com/defined/mobile/ui/AllergyPage.kt
@@ -1,7 +1,6 @@
 package com.defined.mobile.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
